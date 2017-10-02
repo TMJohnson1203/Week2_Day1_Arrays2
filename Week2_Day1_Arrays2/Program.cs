978a8ff8bd66eeb0e1c1bfab79822a26205e206b
TMJohnson1203 <tmjohnson1203@gmail.com>
2017-10-02 11:34:05 -0400

    second commit

--- Week2_Day1_Arrays2/Program.cs
+++ Week2_Day1_Arrays2/Program.cs
@@ -10,9 +10,27 @@ namespace Week2_Day1_Arrays2
     {
         static void Main(string[] args)
         {
-           
-            int[] favNumbers  = { 1, 2, 3, 4, 5, 6 };
-            
+
+            int[] favNumbers = { 61, 22, 3, 44, 105, 36 };
+            string[] studentNames = { "Raz", "Monica", "Rashida", "Teresa", "Brad", "Mia", "Murphy" };
+
+            Console.WriteLine(favNumbers[0]);
+            Console.WriteLine(studentNames[3]);
+
+            string[] topVacaSpots = { "Italy", "Las Vegas", "New Orleans", "Paris", "New York City", "Tampa", "Chicago", "Taos", "San Diego", "London" };
+            Console.WriteLine(topVacaSpots[0] + " " + topVacaSpots[9]);
+
+            int[] childAge = { 5, 8, 12 };
+            Console.WriteLine(childAge[0] + " " + childAge[1] + " " + childAge[2]);
+
+            double[] studentsgpa = { 3.9d, 3.3d, 3.2d, 2.9d, 3.5d, 2.5d, 2.3d, 3.1d };
+            Console.WriteLine(studentsgpa[0]);
+
+            string[] firstLetter = { "R", "D", "J", "L" };
+            Console.WriteLine(firstLetter[0] + " " + firstLetter[1] + " " + firstLetter[2] + " " + firstLetter[3]);
+
+
+
 
 
 
