641151e5f7e1d4a8e3de9ebbf16378c6af59f905
TMJohnson1203 <tmjohnson1203@gmail.com>
2017-10-02 10:20:16 -0400

    initial commit

---
+++ Week2_Day1_Arrays2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2_Day1_Arrays2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int[] favNumbers  = { 1, 2, 3, 4, 5, 6 };
+            
+
+
+
+
+        }
+    }
+}
